ea89f7943031a4804911e75c25b487c6ca4d85f1
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2015-03-09 09:40:46 -0500

    Update token_generation.js
    work on name attribute, instead of hardcoded ID attribute

--- token_generation.js
+++ token_generation.js
@@ -5,8 +5,8 @@ Browser.userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, 
 
 browser.visit("https://app.halowaypoint.com/oauth/spartanToken", function() {
   browser.
-    fill("#i0116", "xx@live.com").
-    fill("#i0118", "xx").
+    fill('input[name="email"]', "xx@live.com").
+    fill('input[name="passwd"]', "xx").
     pressButton("Sign in", function() {
       console.log(browser.text("body"));
       browser.close();
